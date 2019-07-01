4387fd68ce220efa929cc0d0d923bc3287d3bdbe
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-07-01 18:31:01 +0300

    Updates

---
+++ precache-manifest.d59508754f26fa627659d20b98f34932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d025e626ca89a76ca933fca935c2b1",
+    "url": "/cra-demo-todolist/index.html"
+  },
+  {
+    "revision": "5add6be261b14235cbac",
+    "url": "/cra-demo-todolist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cra-demo-todolist/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5add6be261b14235cbac",
+    "url": "/cra-demo-todolist/static/js/main.520714bb.chunk.js"
+  },
+  {
+    "revision": "af3afa24111e6d7943a4",
+    "url": "/cra-demo-todolist/static/js/runtime~main.63a92c20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-demo-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
